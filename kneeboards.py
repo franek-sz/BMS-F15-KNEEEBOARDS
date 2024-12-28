2fdef6861adb372ea75fa6b38c6059f5c311d723
f0v3r <93454655+f0v3r@users.noreply.github.com>
2024-12-28 21:28:21 +0100

    swapped it again

--- kneeboards.py
+++ kneeboards.py
@@ -1,23 +1,16 @@
 import os
 import shutil
 
-# Get the directory where the script is located
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Define the replacements
-replacements = {
-    "7982.dds": "1410.dds",
-    "7983.dds": "1411.dds",
-}
+source_range = range(7982, 7998)
+destination_range = range(1403, 1419)
 
-# Perform the replacements
-for source, destination in replacements.items():
-    source_file = os.path.join(script_directory, source)
-    destination_file = os.path.join(script_directory, destination)
+for src, dest in zip(source_range, destination_range):
+    source_file = os.path.join(script_directory, f"{src}.dds")
+    destination_file = os.path.join(script_directory, f"{dest}.dds")
 
-    # Check if the source file exists
     if os.path.exists(source_file):
-        # Copy the source file to overwrite the destination
         shutil.copyfile(source_file, destination_file)
         print(f"Copied {source_file} to {destination_file}")
     else:
